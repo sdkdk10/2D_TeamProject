8a460d6e7357e2bd19da4e4d586124b058d0a2d6
sdkdk10 <1995jihee@hanmail.net>
2017-10-11 11:40:25 +0900

    인공지능 과제
    Boy 1000명 인공지능

--- homework/sdkdk10/2D_Framework/main_state.py
+++ homework/sdkdk10/2D_Framework/main_state.py
@@ -1,9 +1,19 @@
 import game_framework
 import title_state
+import random
+import numbers
+
 from pico2d import *
 
 name = "TitleState"
 image = None
+
+global curIndex
+curIndex = -1
+global boyIndex
+boyIndex = -1
+
+global mouseX, mouseY
 
 class Grass:
     def __init__(self):
@@ -13,43 +23,141 @@ class Grass:
         self.image.draw(400, 30)
 
 class Boy:
+    image = None
+
+    LEFT_RUN, RIGHT_RUN, LEFT_STAND, RIGHT_STAND = 0, 1, 2, 3
+
     def __init__(self):
-       # self.x, self.y = random.randint(100, 700), 90
-       # self.frame = random.randint(0, 7)
-       self.x, self.y = 100, 90
-       self.frame = 0
-       self.image = load_image('run_animation.png')
+       self.x, self.y = random.randint(100, 700), 90
+       self.frame = random.randint(0, 7)
+       self.dir = 1
+       self.state = self.RIGHT_RUN
+       self.run_frames = 0
+       self.stand_frames = 0
+       if Boy.image == None:
+           Boy.image = load_image('animation_sheet.png')
 
     def update(self):
         self.frame = (self.frame + 1) % 8
-        self.x += 5
+        self.handle_state[self.state](self)
+        #if self.state == self.RIGHT_RUN:
+        #    self.frame = (self.frame + 1) % 8
+        #    self.x += (self.dir * 5)
+        #elif self.state == self.LEFT_RUN:
+        #    self.frame = (self.frame + 1) % 8
+        #    self.x += (self.dir * 5)
+
+        #if self.x > 800:
+        #    self.dir = -1
+        #    self.x = 800
+        #    self.state = self.LEFT_RUN
+        #elif self.x < 0:
+        #    self.dir = 1
+        #    self.x = 0
+        #    self.state = self.RIGHT_RUN
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 100, self.state * 100, 100, 100, self.x, self.y)
+
+    def move(self):
+        global mouseX, mouseY
+        self.x = mouseX
+        self.y = mouseY
+
+    def handle_left_run(self):
+        self.x -= 5
+        self.run_frames += 1
+        if self.x < 0:
+            self.state = self.RIGHT_RUN
+            self.x = 0
+        if self.run_frames == 100:
+            self.state = self.LEFT_STAND
+            self.stand_frames = 0
+
+    def handle_left_stand(self):
+        self.stand_frames += 1
+        if self.stand_frames == 50:
+            self.state = self.LEFT_RUN
+            self.run_frames = 0
+
+    def handle_right_run(self):
+        self.x += 5
+        self.run_frames += 1
+        if self.x > 800:
+            self.state = self.LEFT_RUN
+            self.x = 800
+        if self.run_frames == 100:
+            self.state = self.RIGHT_STAND
+            self.stand_frames = 0
+
+    def handle_right_stand(self):
+        self.stand_frames += 1
+        if self.stand_frames == 50:
+            self.state = self.RIGHT_RUN
+            self.run_frames = 0
+
+    handle_state = {
+        LEFT_RUN: handle_left_run,
+        RIGHT_RUN: handle_right_run,
+        LEFT_STAND: handle_left_stand,
+        RIGHT_STAND: handle_right_stand
+    }
+
+
 
 def enter():
-    global boy, grass
-    boy = Boy()
+    global team, grass
+    team = [Boy() for i in range(1000)]
     grass = Grass()
 
 def exit():
-    global boy, grass
-    del(boy)
+    global team, grass
+    for boy in team:
+        del(boy)
     del(grass)
+
+
 
 def handle_events():
     events = get_events()
+    global mouseX, mouseY
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_SPACE:
+                global curIndex
+                curIndex = 0
+                global boyIndex
+                boyIndex = curIndex
+            elif event.key == SDLK_DOWN:
+                curIndex -= 1
+                if curIndex < 0:
+                    curIndex = 0
+                boyIndex = curIndex
+            elif event.key == SDLK_UP:
+                curIndex += 1
+                if curIndex > 1000:
+                    curIndex = 1000
+                boyIndex = curIndex
+        elif event.type == SDL_MOUSEMOTION:
+            mouseX, mouseY = event.x, 600 - event.y
 
 def update():
-    boy.update()
+    for boy in team:
+        boy.update()
+    delay(0.05)
 
 def draw():
     clear_canvas()
     grass.draw()
-    boy.draw()
+    global boyIndex
+    if boyIndex > 0:
+        team[boyIndex].move()
+        boyIndex = -1
+    for boy in team:
+        boy.draw()
+    numbers.draw(curIndex + 1, 740, 540)
     update_canvas()
